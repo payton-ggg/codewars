de4d9c6f781fb6df48738857132071482dedf04e
Platon <platon20081019@gmail.com>
2025-02-23 23:23:45 +0200

    start

--- 5 kyu/ROT13.js
+++ 5 kyu/ROT13.js
@@ -1,1 +1,32 @@
-function rot13(str) {}
+function rot13(str) {
+  const rot = {
+    a: "n",
+    b: "o",
+    c: "p",
+    d: "q",
+    e: "r",
+    f: "s",
+    g: "t",
+    h: "u",
+    i: "v",
+    j: "w",
+    k: "x",
+    l: "y",
+    m: "z",
+    n: "a",
+    o: "b",
+    p: "c",
+    q: "d",
+    r: "e",
+    s: "f",
+    t: "g",
+    u: "h",
+    v: "i",
+    w: "j",
+    x: "k",
+    y: "l",
+    z: "m",
+  };
+  str.split("");
+}
+console.log();
