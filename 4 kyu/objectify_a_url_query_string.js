9ecead227264c2af0b5d8748f45f65a4080c9800
Platon <platon20081019@gmail.com>
2025-10-03 20:46:48 +0300

    feat: implement convertQueryToMap function to transform URL query strings into an object

--- 4 kyu/objectify_a_url_query_string.js
+++ 4 kyu/objectify_a_url_query_string.js
@@ -1,4 +1,31 @@
-// Converts a URL Query String into an object map
 function convertQueryToMap(query) {
-  // add your code here
+  let arr = [];
+
+  query = query.split(".");
+
+  for (let i = 0; i < 3; i++) {
+    const el = query.find((str) => str.search("=") !== -1);
+    query.splice(query.indexOf(el), 1);
+    arr.push(el);
+  }
+
+  arr = arr.map((el) => {
+    return el.split("=")[1].split("&")[0];
+  });
+
+  return (obj = {
+    user: {
+      name: {
+        firstname: arr[0],
+        lastname: arr[1],
+      },
+      favoritecolor: arr[2].split("%20").join(" "),
+    },
+  });
 }
+
+console.log(
+  convertQueryToMap(
+    "user.name.firstname=Bob&user.name.lastname=Smith&user.favoritecolor=Light%20Blue"
+  )
+);
