411b17b4e2ab0d1b72e95a421a9f2995247d5540
Platon <111748152+payton-ggg@users.noreply.github.com>
2025-12-12 21:19:20 +0200

    Update it_this_a_triangle.js

--- 7 kyu/it_this_a_triangle.js
+++ 7 kyu/it_this_a_triangle.js
@@ -1,5 +1,7 @@
 function isTriangle(a,b,c) {
    const arr = [a, b, c].sort((x, y) => x - y )
+   if (arr[0] <= 0) return false
    return arr[2] <= arr[0] + arr[1]
 }
+
 console.log(isTriangle(2, 4, 5))
