d05ad3e4cad8b26ed36164ed3b0656962e9ff5fe
Platon <111748152+payton-ggg@users.noreply.github.com>
2025-12-14 20:08:43 +0200

    Update list_filtering.js

--- 7 kyu/list_filtering.js
+++ 7 kyu/list_filtering.js
@@ -1,3 +1,7 @@
 function filter_list(l) {
-  // Return a new array with the strings filtered out
+  const arr = [] 
+  l.forEach(el => typeof el === typeof 1 ? arr.push(el) : null)
+  return arr
 }
+
+console.log(filter_list([2, 4, -1, "2"]))
