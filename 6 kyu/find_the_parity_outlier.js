ddf9823cd0a2d3f0951f65c084f0708ee0f69c2e
Platon <111748152+payton-ggg@users.noreply.github.com>
2025-12-21 18:01:12 +0200

    Update find_the_parity_outlier.js

--- 6 kyu/find_the_parity_outlier.js
+++ 6 kyu/find_the_parity_outlier.js
@@ -1,3 +1,7 @@
-function findOutlier(integers){
-  
+function findOutlier(numbers) {
+  const evenCount = numbers.slice(0, 3).filter(n => n % 2 === 0).length;
+  const lookForEven = evenCount < 2;
+
+  return numbers.find(n => (n % 2 === 0) === lookForEven);
 }
+console.log(findOutlier([1, 3, 5, 7]))
